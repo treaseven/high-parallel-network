18c8f2e631374a14b354b4eb601f78f3805f0bf7
zhaokai <1468775486@qq.com>
2024-04-05 15:26:10 +0800

    test InetAddress Socket

---
+++ cla_test/InetAddress.cpp
@@ -0,0 +1,39 @@
+#include "InetAddress.h"
+
+InetAddress::InetAddress()
+{
+    
+}
+
+InetAddress::InetAddress(const std::string &ip, uint16_t port)
+{
+    addr_.sin_family = AF_INET;
+    addr_.sin_addr.s_addr = inet_addr(ip.c_str());
+    addr_.sin_port = htons(port);
+}
+InetAddress::InetAddress(const sockaddr_in addr):addr_(addr)
+{
+
+}
+InetAddress::~InetAddress()
+{
+
+}
+
+const char* InetAddress::ip() const
+{
+    return inet_ntoa(addr_.sin_addr);
+}
+uint16_t InetAddress::port() const
+{
+    return ntohs(addr_.sin_port);
+}
+const sockaddr * InetAddress::addr() const
+{
+    return (sockaddr *)&addr_;
+}
+
+void InetAddress::setaddr(sockaddr_in clientaddr)
+{
+    addr_ = clientaddr;
+}
---
+++ cla_test/Socket.cpp
@@ -0,0 +1,82 @@
+#include "Socket.h"
+
+int createnonblocking()
+{
+    int listenfd = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, IPPROTO_TCP);
+    if (listenfd < 0)
+    {
+        printf("%s:%s:%d listen socket create error:%d\n", __FILE__, __FUNCTION__, __LINE__, errno);
+        exit(-1);
+    }
+    return listenfd;
+}
+
+Socket::Socket(int fd):fd_(fd)
+{
+
+}
+
+Socket::~Socket()
+{
+    ::close(fd_);
+}
+
+int Socket::fd() const
+{
+    return fd_;
+}
+
+void Socket::setreuseaddr(bool on)
+{
+    int optval = on?1:0;
+    ::setsockopt(fd_, SOL_SOCKET, SO_REUSEADDR, &optval, static_cast<socklen_t>(sizeof optval));
+}
+
+void Socket::setreuseport(bool on)
+{
+    int optval = on?1:0;
+    ::setsockopt(fd_, SOL_SOCKET, SO_REUSEPORT, &optval, static_cast<socklen_t>(sizeof optval));
+}
+
+void Socket::settcpnodelay(bool on)
+{
+    int optval = on?1:0;
+    ::setsockopt(fd_, SOL_SOCKET, TCP_NODELAY, &optval, static_cast<socklen_t>(sizeof optval));
+}
+
+void Socket::setkeepalive(bool on)
+{
+    int optval = on?1:0;
+    ::setsockopt(fd_, SOL_SOCKET, SO_KEEPALIVE, &optval, static_cast<socklen_t>(sizeof optval));
+}
+
+void Socket::bind(const InetAddress& servaddr)
+{
+    if (::bind(fd_, servaddr.addr(), sizeof(sockaddr)) < 0)
+    {
+        perror("bind() failed");
+        close(fd_);
+        exit(-1);
+    }
+}
+
+void Socket::listen(int nn)
+{
+    if (::listen(fd_, nn) != 0)
+    {
+        perror("listen() failed");
+        close(fd_);
+        exit(-1);
+    }
+}
+
+int Socket::accept(InetAddress& clientaddr)
+{
+    struct sockaddr_in peeraddr;
+    socklen_t len = sizeof(peeraddr);
+    int clientfd = accept4(fd_, (struct sockaddr *)&peeraddr, &len, SOCK_NONBLOCK);
+
+    clientaddr.setaddr(peeraddr);
+
+    return clientfd;
+}
